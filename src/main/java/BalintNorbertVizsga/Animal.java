4f9b8c0b1dab7c093b3aa9dd1cc104857a05ad0f
Bálint Norbert <norbaa79@gmail.com>
2023-12-24 16:02:22 +0100

    Creating a new cat and dog class and instantiating main

---
+++ src/main/java/BalintNorbertVizsga/Animal.java
@@ -0,0 +1,21 @@
+package BalintNorbertVizsga;
+
+public class Animal {
+
+    public String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+
+    };
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
--- src/main/java/BalintNorbertVizsga/Main.java
+++ src/main/java/BalintNorbertVizsga/Main.java
@@ -2,6 +2,16 @@ package BalintNorbertVizsga;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+
+        Dog dog = new Dog("Blöki");
+        System.out.println(dog.name +" mondja: ");
+        dog.makeSound();
+
+        System.out.println("----------------");
+
+        Cat cat = new Cat("Cicus");
+        System.out.println(cat.name +" mondja: ");
+        cat.makeSound();
+
     }
 }
